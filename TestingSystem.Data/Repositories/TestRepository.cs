87f884690b9670226e3c3b0ef610e5d3ef3bbdc6
duc <31963396+Ducluongchung@users.noreply.github.com>
2019-03-20 09:48:16 +0700

    20/3
    ad

---
+++ TestingSystem.Data/Repositories/TestRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using TestingSystem.Data.Infrastructure;
+using TestingSystem.Models;
+
+namespace TestingSystem.Data.Repositories
+{
+	public interface ITestRepository :IRepository<Test>
+	{
+
+	}
+	public class TestRepository
+	{
+		
+	}
+}
