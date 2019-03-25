611a4303c99a2a7b81388a6e3d3762fcfbb40315
vananh <anhvv13@gmail.com>
2019-03-25 15:20:55 +0700

    2503 commit

--- TestingSystem/Controllers/ExamController.cs
+++ TestingSystem/Controllers/ExamController.cs
@@ -3,22 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TestingSystem.BaseController;
 using TestingSystem.Data.Repositories;
+using TestingSystem.Sevice;
 
 namespace TestingSystem.Controllers
 {
-	public class ExamController : Controller
+	public class ExamController : ClientController
 	{
-		private readonly IExamPaperRepository examPaperRepository;
-		public ExamController(IExamPaperRepository ExamPaperRepository)
+		private readonly IExamPaperService examPaperService;
+        private readonly IExamPaperExamService examPaperExamService;
+		public ExamController(IExamPaperService examPaperService, IUserService userService, IExamPaperExamService examPaperExamService):base(userService)
 		{
-			this.examPaperRepository = ExamPaperRepository;
-		}
+            this.examPaperExamService = examPaperExamService;
+
+            this.examPaperService = examPaperService;
+        }
 		// GET: Exam
 		public ActionResult Index()
 		{
-			var model = examPaperRepository.ListExamPapersTop();
-			return PartialView(model);
+			var model = examPaperService.ListExamPapersTop();
+
+            return PartialView(model);
 		}
 
 	}
--- TestingSystem/Controllers/ExamPaperController.cs
+++ TestingSystem/Controllers/ExamPaperController.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TestingSystem.BaseController;
 using TestingSystem.Sevice;
 
 namespace TestingSystem.Controllers
 {
-    public class ExamPaperController : Controller
+    public class ExamPaperController : AdminController
     {
 	    public IExamPaperService examPaperService;
 		// GET: ExamPaper
-		public ExamPaperController(IExamPaperService examPaperService)
+		public ExamPaperController(IExamPaperService examPaperService, IUserService userService):base(userService)
 		{
 			this.examPaperService = examPaperService;
 		}
